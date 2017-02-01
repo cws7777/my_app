c5b2ffc49c82cf70dec225305a0051721db61923
cws7777 <cws7777@hotmail.com>
2017-02-01 14:59:22 +0900

    Added tables and buttons

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 //connect database
 mongoose.connect(process.env.MONGO_DB);
@@ -30,21 +31,50 @@ app.set("view engine", 'ejs');
 //set middlewares
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 //set routes
 app.get('/posts', function(req, res){
-  Post.find({}, function(err, posts){
+  Post.find({}).sort('-createdAt').exec(function(err, posts){
     if(err) return res.json({success:false, message: err});
-    res.json({success:true, data:posts});
+    res.render("posts/index", {data:posts});
   });
 }); //index
+app.get('/posts/new', function(req, res){
+  res.render("posts/new");
+}); // new
 app.post('/posts', function(req, res){
   Post.create(req.body.post, function(err, post){
     if(err) return res.json({success:false, message:err});
-    res.json({success:true, data:post});
+    res.redirect('/posts');
   });
-});// create
-
+}); // create
+app.get('/posts/:id', function(req,res){
+  Post.findById(req.params.id, function (err, post){
+    if (err) return res.json({success:false, message: err});
+    res.render("posts/show", {data:post});
+  });
+}); // show
+app.get('/posts/:id/edit', function(req,res){
+  Post.findById(req.params.id, function (err,post) {
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/edit", {data:post});
+  });
+}); // edit
+app.put('/posts/:id', function(req,res){
+  req.body.post.updatedAt = Date.now();
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function(err, post){
+    if(err) return res.json({success:false, message:err});
+    res.redirect('/posts/'+req.params.id);
+  });
+}); //update
+app.delete('/posts/:id', function(req,res){
+  Post.findByIdAndRemove(req.params.id, function(err, post){
+    if(err) return res.json({success:false, message:err});
+    res.redirect('/posts');
+  });
+}); // destroy
 //start server
 app.listen(3000, function(){
   console.log('Server On!!!');
