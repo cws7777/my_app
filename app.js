4c1c1bfc227fb049bb091b2c9d54b1a826caf0a8
cws7777 <cws7777@hotmail.com>
2017-02-01 13:31:40 +0900

    Set middlewares routes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+//import modules
 var express = require('express');
 var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
+//connect database
 mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
 db.once("open", function(){
@@ -12,6 +15,41 @@ db.on("error", function(err){
   console.log("DB ERROR :", err);
 });
 
+//model setting
+var postSchema = mongoose.Schema({
+  title: {type:String, required:true},
+  body: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now},
+  updatedAt: Date
+});
+var Post = mongoose.model('post',postSchema);
+
+//view setting
+app.set("view engine", 'ejs');
+
+//set middlewares
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+//set routes
+app.get('/posts', function(req, res){
+  Post.find({}, function(err, posts){
+    if(err) return res.json({success:false, message: err});
+    res.json({success:true, data:posts});
+  });
+}); //index
+app.post('/posts', function(req, res){
+  Post.create(req.body.post, function(err, post){
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, data:post});
+  });
+});// create
+
+//start server
+app.listen(3000, function(){
+  console.log('Server On!!!');
+});
+/*
 var dataSchema = mongoose.Schema({
   name:String,
   count:Number
@@ -69,6 +107,4 @@ function getCounter(res){
     res.render('my_first_ejs', data);
   });
 }
-app.listen(3000, function(){
-  console.log('Server On!!!');
-});
+*/
