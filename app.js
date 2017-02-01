c70c4df610234509f21a7fb2324eb5a039dab974
cws7777 <cws7777@hotmail.com>
2017-02-01 16:22:41 +0900

    Added some features of logging in

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-//import modules
-var express = require('express');
-var path = require('path');
-var app = express();
+// import modules
+var express  = require('express');
+var app      = express();
+var path     = require('path');
 var mongoose = require('mongoose');
 var passport = require('passport');
-var session = require('express-session');
-var flash = require('connect-flash');
-var async = require('async');
-var bodyParser = require('body-parser');
+var session  = require('express-session');
+var flash    = require('connect-flash');
+var async    = require('async');
+var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 
-//connect database
+// connect database
 mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
-db.once("open", function(){
-  console.log("DB connected");
+db.once("open",function () {
+  console.log("DB connected!");
 });
-db.on("error", function(err){
+db.on("error",function (err) {
   console.log("DB ERROR :", err);
 });
 
-//model setting
+// model setting
 var postSchema = mongoose.Schema({
   title: {type:String, required:true},
   body: {type:String, required:true},
@@ -30,17 +30,17 @@ var postSchema = mongoose.Schema({
 var Post = mongoose.model('post',postSchema);
 
 var userSchema = mongoose.Schema({
-  email: {type:String, required: true, unique:true},
+  email: {type:String, required:true, unique:true},
   nickname: {type:String, required:true, unique:true},
-  password: {type:String, required:true, unique:true},
-  createdAt: {type:Date, default: Date.now}
+  password: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now}
 });
 var User = mongoose.model('user',userSchema);
 
-//view setting
+// view setting
 app.set("view engine", 'ejs');
 
-//set middlewares
+// set middlewares
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -51,11 +51,11 @@ app.use(session({secret:'MySecret'}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser(function(user, done){
+passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
-passport.deserializeUser(function(id, done){
-  User.findById(id, function(err, user){
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
     done(err, user);
   });
 });
@@ -63,43 +63,43 @@ passport.deserializeUser(function(id, done){
 var LocalStrategy = require('passport-local').Strategy;
 passport.use('local-login',
   new LocalStrategy({
-    usernameField : 'email',
-    passwordField : 'password',
-    passReqToCallback : true
-  },
-  function(req, email, password, done){
-    User.findOne({'email': email}, function(err, user){
-      if(err) return done(err);
+      usernameField : 'email',
+      passwordField : 'password',
+      passReqToCallback : true
+    },
+    function(req, email, password, done) {
+      User.findOne({ 'email' :  email }, function(err, user) {
+        if (err) return done(err);
 
-      if(!user){
-        req.flash("email", req.body.email);
-        return done(null, false, req.flash('loginError', 'No user found'));
-      }
-      if(user.password != password){
-        req.flash("email", req.body.email);
-        return done(null, false, req.flash('loginError', 'Password does not match.'));
-      }
-      return done(null, user);
-    });
-  }
-)
+        if (!user){
+            req.flash("email", req.body.email);
+            return done(null, false, req.flash('loginError', 'No user found.'));
+        }
+        if (user.password != password){
+            req.flash("email", req.body.email);
+            return done(null, false, req.flash('loginError', 'Password does not Match.'));
+        }
+        return done(null, user);
+      });
+    }
+  )
 );
 
-//set home routes
-app.get('/', function(req, res){
+// set home routes
+app.get('/', function (req,res) {
   res.redirect('/posts');
 });
-app.get('/login', function(req, res){
-  res.render('login/login', {email:req.flash("email")[0], loginError:req.flash('loginError')});
+app.get('/login', function (req,res) {
+  res.render('login/login',{email:req.flash("email")[0], loginError:req.flash('loginError')});
 });
 app.post('/login',
-  function(req,res,next){
-    req.flash("email");  //flash email data
+  function (req,res,next){
+    req.flash("email"); // flush email data
     if(req.body.email.length === 0 || req.body.password.length === 0){
       req.flash("email", req.body.email);
-      req.flash("loginError", "Please enter both email and password");
+      req.flash("loginError","Please enter both email and password.");
       res.redirect('/login');
-    } else{
+    } else {
       next();
     }
   }, passport.authenticate('local-login', {
@@ -108,46 +108,46 @@ app.post('/login',
     failureFlash : true
   })
 );
-app.get('/logout', function(req,res){
-  req.logout();
-  res.redirect('/');
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
 });
 
-//set user routes
+// set user routes
 app.get('/users/new', function(req,res){
-  res.render('user/new', {
+  res.render('users/new', {
                             formData: req.flash('formData')[0],
                             emailError: req.flash('emailError')[0],
                             nicknameError: req.flash('nicknameError')[0],
-                            passwordError: req.flash('passwordError')[0],
+                            passwordError: req.flash('passwordError')[0]
                           }
   );
-});  //new
-app.post('/users', checkUserRegValidation, function(req, res, next){
-  User.create(req.body.user, function(err, user){
-    if(err) return res.json({success:false, message: err});
+}); // new
+app.post('/users', checkUserRegValidation, function(req,res,next){
+  User.create(req.body.user, function (err,user) {
+    if(err) return res.json({success:false, message:err});
     res.redirect('/login');
   });
-});  //create
-app.get('users/:id', function(req, res){
-  User.findById(req.params.id, function(err, user){
-    if(err) return res.json({success:false, message: err});
-    res.render("users/show", {user: user});
-  });
-});  //show
-app.get('/users/:id/edit', function(req, res){
-  User.findById(req.params.id, function(err, user){
-    if (err) return res.json({success:false, message: err});
+}); // create
+app.get('/users/:id', function(req,res){
+    User.findById(req.params.id, function (err,user) {
+      if(err) return res.json({success:false, message:err});
+      res.render("users/show", {user: user});
+    });
+}); // show
+app.get('/users/:id/edit', function(req,res){
+  User.findById(req.params.id, function (err,user) {
+    if(err) return res.json({success:false, message:err});
     res.render("users/edit", {
-                              user : user,
-                              formData : req.flash('formData')[0],
+                              user: user,
+                              formData: req.flash('formData')[0],
                               emailError: req.flash('emailError')[0],
                               nicknameError: req.flash('nicknameError')[0],
                               passwordError: req.flash('passwordError')[0]
-                            }
+                             }
     );
   });
-});  //edit
+}); // edit
 app.put('/users/:id', checkUserRegValidation, function(req,res){
   User.findById(req.params.id, req.body.user, function (err,user) {
     if(err) return res.json({success:"false", message:err});
@@ -169,26 +169,26 @@ app.put('/users/:id', checkUserRegValidation, function(req,res){
   });
 }); //update
 
-//set posts routes
-app.get('/posts', function(req, res){
-  Post.find({}).sort('-createdAt').exec(function(err, posts){
-    if(err) return res.json({success:false, message: err});
+// set posts routes
+app.get('/posts', function(req,res){
+  Post.find({}).sort('-createdAt').exec(function (err,posts) {
+    if(err) return res.json({success:false, message:err});
     res.render("posts/index", {data:posts, user:req.user});
   });
-}); //index
-app.get('/posts/new', function(req, res){
+}); // index
+app.get('/posts/new', function(req,res){
   res.render("posts/new");
 }); // new
-app.post('/posts', function(req, res){
+app.post('/posts', function(req,res){
   console.log(req.body);
-  Post.create(req.body.post, function(err, post){
+  Post.create(req.body.post,function (err,post) {
     if(err) return res.json({success:false, message:err});
     res.redirect('/posts');
   });
 }); // create
 app.get('/posts/:id', function(req,res){
-  Post.findById(req.params.id, function (err, post){
-    if (err) return res.json({success:false, message: err});
+  Post.findById(req.params.id, function (err,post) {
+    if(err) return res.json({success:false, message:err});
     res.render("posts/show", {data:post});
   });
 }); // show
@@ -199,18 +199,18 @@ app.get('/posts/:id/edit', function(req,res){
   });
 }); // edit
 app.put('/posts/:id', function(req,res){
-  req.body.post.updatedAt = Date.now();
-  Post.findByIdAndUpdate(req.params.id, req.body.post, function(err, post){
+  req.body.post.updatedAt=Date.now();
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function (err,post) {
     if(err) return res.json({success:false, message:err});
     res.redirect('/posts/'+req.params.id);
   });
 }); //update
 app.delete('/posts/:id', function(req,res){
-  Post.findByIdAndRemove(req.params.id, function(err, post){
+  Post.findByIdAndRemove(req.params.id, function (err,post) {
     if(err) return res.json({success:false, message:err});
     res.redirect('/posts');
   });
-}); // destroy
+}); //destroy
 
 //functions
 function checkUserRegValidation(req, res, next) {
@@ -248,9 +248,10 @@ function checkUserRegValidation(req, res, next) {
     }
   );
 }
-//start server
+
+// start server
 app.listen(3000, function(){
-  console.log('Server On!!!');
+  console.log('Server On!');
 });
 /*
 var dataSchema = mongoose.Schema({
