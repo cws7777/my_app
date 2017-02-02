fcc27a03edf39a145c1a092a5eb6780ce2079de8
cws7777 <cws7777@hotmail.com>
2017-02-02 13:52:17 +0900

    Simplified the code in app.js

--- app.js
+++ app.js
@@ -210,35 +210,29 @@ app.post('/posts', isLoggedIn, function(req,res){
 app.get('/posts/:id', function(req,res){
   Post.findById(req.params.id).populate("author").exec(function (err,post) {
     if(err) return res.json({success:false, message:err});
-    res.render("posts/show", {data:post, user:req.user});
+    res.render("posts/show", {post:post, user:req.user});
   });
 }); // show
 app.get('/posts/:id/edit', isLoggedIn, function(req,res){
   Post.findById(req.params.id, function (err,post) {
     if(err) return res.json({success:false, message:err});
     if(!req.user._id.equals(post.author)) return res.json({success:false, message:"Unauthorized Attempt"});
-    res.render("posts/edit", {data:post, user:req.user});
+    res.render("posts/edit", {post:post, user:req.user});
   });
 }); // edit
 app.put('/posts/:id', isLoggedIn, function(req,res){
   req.body.post.updatedAt=Date.now();
-  Post.findById(req.params.id, function (err,post) {
+  Post.findOneAndUpdate({_id:req.params.id, author:req.user._id}, req.body.post, function (err,post) {
     if(err) return res.json({success:false, message:err});
-    if(!req.user._id.equals(post.author)) return res.json({success:false, message:"Unauthorized Attempt"});
-    Post.findByIdAndUpdate(req.params.id, req.body.post, function(err, post){
-      if(err) return res.json({success:false, message:err});
-      res.redirect('/posts/'+req.params.id);
-    });
+    if(!post) return res.json({success:false, message: "No data found to update"});
+    res.redirect('/posts/'+req.params.id);
   });
 }); //update
 app.delete('/posts/:id', isLoggedIn, function(req,res){
-  Post.findById(req.params.id, function (err,post) {
+  Post.findOneAndRemove({_id:req.params.id, author:req.user._id}, req.body.post, function (err,post) {
     if(err) return res.json({success:false, message:err});
-    if(!req.user._id.equals(post.author)) return res.json({success:false, message:"Unauthorized Attempt"});
-    Post.findByIdAndRemove(req.params.id, function (err, post){
-      if(err) return res.json({success:false, message:err});
-      res.redirect('/posts');
-    });
+    if(!post) return res.json({success:false, message: "No data found to delete"});
+    res.redirect('/posts');
   });
 }); //destroy
 
