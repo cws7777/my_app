3c7aac2fc70471c5ae19fb4ee7f428653abfff92
cws7777 <cws7777@hotmail.com>
2017-02-02 15:45:46 +0900

    Redesign all of things

--- models/User.js
+++ models/User.js
@@ -7,7 +7,32 @@ var userSchema = mongoose.Schema({
   password: {type:String, required:true},
   createdAt: {type:Date, default:Date.now}
 });
-userSchema.pre("save", function (next){
+userSchema.pre("save", hashPassword);
+userSchema.pre("findOneAndUpdate", function hashPassword(next){
+  console.log(this._update);
+  var user = this._update;
+  if(!user.newPassword){
+    delete user.password;
+    return next();
+  } else {
+    user.password = bcrypt.hashSync(user.newPassword);
+    return next();
+  }
+});
+
+userSchema.methods.authenticate = function (password) {
+  var user = this;
+  return bcrypt.compareSync(password,user.password);
+};
+userSchema.methods.hash = function (password) {
+  return bcrypt.hashSync(password);
+};
+var User = mongoose.model('user',userSchema);
+
+module.exports = User;
+
+function hashPassword(next){
+  console.log("hi");
   var user = this;
   if(!user.isModified("password")){
     return next();
@@ -15,14 +40,4 @@ userSchema.pre("save", function (next){
     user.password = bcrypt.hashSync(user.password);
     return next();
   }
-});
-userSchema.methods.authenticate = function (password) {
-  var user = this;
-  return bcrypt.compareSync(password,user.password);
-};
-userSchema.methods.hash = function (password){
-  return bcrypt.hashSync(password);
-};
-var User = mongoose.model('user',userSchema);
-
-module.exports = User;
+}
