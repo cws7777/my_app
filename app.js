d851d0049d7218a0910c18bd8f70c52f4a08d4a8
cws7777 <cws7777@hotmail.com>
2017-02-02 17:10:43 +0900

    Added some comments

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path     = require('path');           //package for directory path.
 var mongoose = require('mongoose');       //the most common package for controlling MONGO_DB in express.js
 var session  = require('express-session');//package that is like a flag which can control user is logged in or out.
 var flash    = require('connect-flash');  //package that can save session data as flash.
-var bodyParser     = require('body-parser');
+var bodyParser     = require('body-parser');    //package that can read and analyze json file.
 var methodOverride = require('method-override');
 
 // database
@@ -20,17 +20,17 @@ db.on("error",function (err) {
 // view engine
 app.set("view engine", 'ejs');          //Noticing to express that ejs will be used as view engine.
 
-// middlewares
+// middlewares which are commands that implements in all signals between router and arriving server.
 app.use(express.static(path.join(__dirname, 'public')));      //Setting static folder directory. path.join makes automatic '/' even developers don't put '/'.
-app.use(bodyParser.json());
+app.use(bodyParser.json());                                   //Read and analyzing json file.
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.use(flash());
-app.use(session({secret:'MySecret'}));
+app.use(session({secret:'MySecret'}));                      //To keep logging in the user. Secret which is secret hash key encrypts session
 
 // passport
 var passport = require('./config/passport');
-app.use(passport.initialize());
+app.use(passport.initialize());       //Initializing and utilizing log in process.
 app.use(passport.session());
 
 // routes
--- config/passport.js
+++ config/passport.js
@@ -2,17 +2,17 @@ var passport      = require('passport');        //passport package is the most c
 var LocalStrategy = require('passport-local').Strategy;
 var User          = require('../models/User');
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function(user, done) {         //Session 생성 시에 어떠한 정보를 저장할지를 설정.
   done(null, user.id);
 });
-passport.deserializeUser(function(id, done) {
+passport.deserializeUser(function(id, done) {         //Session으로부터 개체를 가져올 때 어떻게 가져올 지를 정하는 것.
   User.findById(id, function(err, user) {
     done(err, user);
   });
 });
 
 passport.use('local-login',
-  new LocalStrategy({
+  new LocalStrategy({             //Used local-strategy
       usernameField : 'email',
       passwordField : 'password',
       passReqToCallback : true
